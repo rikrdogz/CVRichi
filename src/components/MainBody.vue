3a9d23433b4f7c8ea8fb10fe7c90284a2b125f96
ricardo <luisricardogz@gmail.com>
2022-04-25 20:48:28 -0500

    custom read data

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -1,25 +1,49 @@
 <template>
   <div>
-    
+
     <CVTitle></CVTitle>
 
-    <div>
-      <exp-work :titleprofile="'Desarrollador de software'"></exp-work>
-    </div>
-    
+    <div v-for="(work, index) in ListWork" :key="index">
 
+      <exp-work
+        :titleprofile="work.titleprofile"
+        :company="work.company"
+        :time="work.time"
+        :resume="work.resume"
+		:listTechs="work.listTechs"
+      ></exp-work>
+
+
+    </div>
   </div>
 </template>
 
 <script>
-import CVTitle from './title/CVTitle.vue';
-import ExpWork from './experience/work.vue';
+import CVTitle from "./title/CVTitle.vue";
+import ExpWork from "./experience/work.vue";
+
+import Spanish from "../assets/json/spanish.json";
 
 export default {
-  name: 'HelloWorld',
-  components: {CVTitle, ExpWork},
+  name: "HelloWorld",
+  components: { CVTitle, ExpWork },
+  data() {
+    return {
+		ListWork:[],
+	};
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  methods: {
+    LoadData() {
+		this.ListWork = Spanish;
+	},
+  },
+
+	created() {
+		this.LoadData();
+	},
+
+};
 </script>
--- src/components/experience/work.vue
+++ src/components/experience/work.vue
@@ -1,8 +1,32 @@
 <template>
-    <div class="p-2 shadow mt-4">
-        <div><strong class="text-2xl">{{titleprofile}}</strong></div>
-        <div v-if="listTechs.lenght > 0">{{listTechs}}</div>
-        <div><span>{{resum}}</span></div>
+    <div class="p-4 shadow mt-4">
+        
+        <div>
+            <strong class="text-2xl">{{titleprofile}}</strong>
+        </div>
+
+        <div v-if="listTechs.length > 0"  class="m-2 ml-0">
+            <span v-for="(word, index) in listTechs" :key="index" class="p-1 border text-blue-400 ml-1">{{word}}</span>
+        </div>
+        
+        <div class="text-slate-700 mt-1">
+            <font-awesome-icon icon="building" />
+            <span class="tracking-wider uppercase  ml-1  font-light"> {{company}} </span>
+        </div>
+
+        <div class="mt-1">
+            <font-awesome-icon icon="calendar" />
+            <span class=" font-light ml-1"> {{time}} </span>
+        </div>
+
+        <div class="flex mt-1">
+            <font-awesome-icon icon="message" />
+            <div class="ml-1">
+                <p v-for="(word, index) in resume" :key="index"> 
+                {{word}}
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -16,8 +40,10 @@ export default {
     },
     props:{
         titleprofile : { type: String, default:''},
-        resum:{type:String, default:''},
+        resume:{type:String, default:''},
         listTechs:{type:Array, default:new Array},
+        company:{type:String, default:''},
+        time:{type:String, default:''}
     }
 }
 </script>
